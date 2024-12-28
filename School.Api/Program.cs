520b1906c9b06e2848b569d0c49e7f0474c4d687
abdulrahman <abdulrahmn398@outlook.com>
2024-12-28 20:43:52 +0200

    update some files and add migration and update database

--- School.Api/Program.cs
+++ School.Api/Program.cs
@@ -1,27 +1,34 @@
-namespace School.Api
+using Microsoft.EntityFrameworkCore;
+using School.Infrustructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Connection to SQL Server
+builder.Services.AddDbContext<AppDbContext>(option =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
-            // Add services to the container.
+var app = builder.Build();
 
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
