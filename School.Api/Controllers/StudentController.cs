e9257f307b727b9e30871ab89e866d45e7df3635
abdulrahman <abdulrahmn398@outlook.com>
2025-03-06 17:16:31 +0200

    Refactor StudentController to use AppControllerBase
    Updated StudentController to inherit from AppControllerBase, enabling shared functionality such as the Mediator and standardized response handling. Removed the private _mediator field and constructor initialization, utilizing the Mediator property instead. Introduced AppControllerBase with lazy initialization of the Mediator and a NewResult method for consistent response formatting. These changes enhance code structure and maintainability.

--- School.Api/Controllers/StudentController.cs
+++ School.Api/Controllers/StudentController.cs
@@ -1,5 +1,5 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using School.Api.Base;
 using School.Core.Features.Students.Commands.Models;
 using School.Core.Features.Students.Queries.Models;
 using School.Data.AppMetaData;
@@ -7,37 +7,28 @@ using School.Data.AppMetaData;
 namespace School.Api.Controllers;
 
 [ApiController]
-public class StudentController : ControllerBase
+public class StudentController : AppControllerBase
 {
-    private readonly IMediator _mediator;
-
-    public StudentController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
 
     [HttpGet(Router.StudentRouting.List)]
     public async Task<IActionResult> GetStudentList()
     {
-        var response = await _mediator
-            .Send(new GetStudentListQuery());
-        return Ok(response);
+        return Ok(await Mediator
+            .Send(new GetStudentListQuery()));
     }
 
     [HttpGet(Router.StudentRouting.GetById)]
     public async Task<IActionResult> GetStudentById([FromRoute] int id)
     {
-        var response = await _mediator
-            .Send(new GetStudentByIdQuery(id));
-        return Ok(response);
+        return NewResult(await Mediator
+            .Send(new GetStudentByIdQuery(id)));
     }
 
     [HttpPost(Router.StudentRouting.Create)]
     public async Task<IActionResult> CreateStudent
         ([FromBody] AddStudentCommand studentCommand)
     {
-        var response = await _mediator
-            .Send(studentCommand);
-        return Ok(response);
+        return NewResult(await Mediator
+            .Send(studentCommand));
     }
 }
